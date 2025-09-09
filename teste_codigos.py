6112f8fb02995e530a499558b8ab19e2ea6fc31a
Breno Ferraz <Ferrazbreno42@outlook.com>
2025-09-09 09:29:53 -0300

    Criação de testes em python

--- teste_codigos.py
+++ teste_codigos.py
@@ -1,1 +1,68 @@
-print("Hello, World !")
+# print("Hello, World !")
+
+# citacoes = ['A vida é bela', 'A vida é uma caixinha de surpresas', 'A vida é um grande aprendizado']
+# print(citacoes)
+
+# valores = [1, 2, 3, 4, 5]
+# print(valores)
+
+# resultado_da_soma = valores[1] + valores[3]
+# print(resultado_da_soma)
+
+# bandas_favoritas = {
+#    'banda_favorita' : 'The Beatles',
+#    'segunda_banda_favorita' : 'Queen',
+#    'terceira_banda_favorita' : 'Nirvana'
+# }
+
+# print(bandas_favoritas)
+# print(bandas_favoritas['banda_favorita'])
+
+# texto_sujo = "   Olá, Mundo!   "
+# texto_limpo = texto_sujo.strip()
+# print(texto_limpo)
+
+# idade = 20
+
+# if idade >= 18:
+#    print("Você pode dirigir")
+# else:
+#     print("Você não pode dirigir")
+
+
+# x = 10
+# y = 5
+
+# igualdade = x == y
+# diferenca = x != y
+
+# print("Igualdade:", igualdade)
+# print("Diferença:", diferenca)
+
+# def saudar():
+#     nome = input("Qual o seu nome ? ")
+#    print(f"Olá! {nome}")
+    
+# saudar()
+
+# def saudar(nome):
+#     print(f"Olá! {nome}")
+    
+# saudar("João")
+
+# def saudar(nome):
+#    return f"Olá! {nome}"
+    
+# mensagem = saudar("João")
+# print(mensagem)
+
+# def saudar(nome: str) -> str:
+#    return f"Olá! {nome}"
+    
+# mensagem = saudar("João")
+# print(mensagem)
+
+# from math import sqrt
+
+# print(sqrt(16))
+
